72fd0a9a0f025b437d93bb66829cdd91ff10a801
BJanssen78 <98946596+BJanssen78@users.noreply.github.com>
2022-06-04 09:51:38 +0200

    updating string

--- operators/main.py
+++ operators/main.py
@@ -3,3 +3,36 @@ __winc_id__ = 'd0d3cdcefbb54bc980f443c04ab3a9eb'
 __human_name__ = 'operators'
 
 # Add your code after this line
+
+#Setting variables
+fav_lang_Spain = 'Spanish'
+fav_lang_Swits = 'Swiss' #Zwitserland
+most_religions_Spain = 'Roman Catholic'
+most_religions_Swits = 'Roman Catholic'
+capital_Spain = 'Madrid'
+capital_Swits = 'Bern'
+gdp_spain = 1714860000000
+gdp_swits = 590710000000
+growth_spain = '1,95%'
+growth_swits = '0,65%'
+populatie_spain = 47163418
+populatie_swits = 8508698
+
+#vergelijkingen
+fav_taal = fav_lang_Swits == fav_lang_Spain #01
+most_religions = most_religions_Spain == most_religions_Swits #02
+length_capital = (len(capital_Spain)!= len(capital_Swits)) #03
+swits_gdp_greater_than = gdp_swits > gdp_spain #04
+overall_growth_spain = growth_spain < '1%'
+overall_growth_swits = growth_swits < '1%'
+population_10mill_spain = populatie_spain > 10000000
+population_10mill_swits = populatie_swits > 10000000
+
+#print uitkomst
+print (fav_taal) #01 false
+print(most_religions) #02 true
+print(swits_gdp_greater_than) #03 false
+print(overall_growth_spain) #04 false
+print(overall_growth_swits) #05 true
+print(population_10mill_spain) #06 
+print(population_10mill_swits)
--- strings/main.py
+++ strings/main.py
@@ -6,50 +6,52 @@ __human_name__ = 'strings'
 
 from gettext import find
 
+#part 1
 
-spelers_NL = ["hans van breukelen", "berry van aerle", "frank rijkaard",
-"ronald koeman", "adri van tiggelen", "gerard vanenburg", "jan wouters",
-"arnold muchen", "erwin koeman", "ruud gullit", "marco van basten"]
-spelers_NL_nr = ["1", "6", "17", "4", "2", "7", "20", "8", "13", "10", "12"]
+#poging 1 van de opdracht strings
+doelpunt_speler = ['Ruud Gullit', 'Marco van Basten']
+doelpunt_tijd = ['32', '54']
 
-spelers_SU = [
-"Rinat Dasayev",
-"Anatoliy Demyanenko",
-"Sergei Aleinikov",
-"Vagiz Khidiyatullin",
-"Vasyl Rats",
-"Hennadiy Lytovchenko",
-"Oleksandr Zavarov",
-"Oleksiy Mykhaylychenko",
-"Sergey Gotsmanov",
-"Oleh Protasov",
-"Igor Belanov"]
-spelers_SU_nr = ["1", "5", "7", "3", "6", "8", "9", "15", "18", "10", "11"]
-
-doelpunt_speler = ["Ruud Gullit", "Marco van Basten"]
-doelpunt_tijd = ["32", "54"]
-
-scorers = (doelpunt_speler[0]) + ' ' + (doelpunt_tijd[0]) + (', ') + (doelpunt_speler[1]) + ' ' + (doelpunt_tijd[1])
-report = (doelpunt_speler[0]) + ' scored in the ' + (doelpunt_tijd[0] + 'nd minute') + ('\n') + (doelpunt_speler[1]) + ' scored in the ' + (doelpunt_tijd[1] + 'th minute')
-
-print (report)
-
-player = 'Gerard Vanenburg'
-first_name = player[:player.find(' ')]
-last_name_len = len(player[player.find(' ')+1:])
-name_short = player[:1] + '. ' + player[7:]
+#poging 2 van de opdracht strings, na de feedback van de leraar.
+scorer1 = 'Ruud Gullit'
+scorer2 = 'Marco van Basten'
 goal_0 = 32
 goal_1 = 54
-goal_2 = len(first_name) -2
 
-chant = first_name + '!' + ' ' +((first_name + '! ') * goal_2) +  first_name +'!'
-chant2 = len(chant[:chant.find('!')])
-chant3 = chant2 * goal_2
-good_chant = chant != 'Gerard! Gerard! Gerard! Gerard! Gerard! Gerard! '
+#scores = een test van mezelf om te zien wat de uitkomst is.
+scorers = (doelpunt_speler[0]) + ' ' + (doelpunt_tijd[0]) + (', ') + (doelpunt_speler[1]) + ' ' + (doelpunt_tijd[1])
+#poging 1 van de opdracht strings
+report = (doelpunt_speler[0]) + ' scored in the ' + (doelpunt_tijd[0] + 'nd minute') + ('\n') + (doelpunt_speler[1]) + ' scored in the ' + (doelpunt_tijd[1] + 'th minute')
+#poging 2 van de opdracht strings, na de feedback van de leraar.
+report2 = f'{scorer1} scored in the {goal_0}nd minute \n{scorer2} scored in the {goal_1}th minute'
 
-print(2 != 3)
-print(2 != 2)
-print (chant)
-print (chant2)
-print (chant3)
-print (spelers_NL+[spelers_NL_nr])
+#In de assignment staat bij part 1 punt 4 dat de F of de + operator gebruikt moet worden.
+#Ik heb nu beide in de code staan.
+
+#poging 1 van de opdracht strings
+print (report + '\n')
+print (scorers + '\n')
+print (report2 + '\n') #poging 2 van de opdracht strings, na de feedback van de leraar.
+
+#Strings part 2
+
+player = 'Gerard Vanenburg' #punt 1
+first_name = player[:player.find(' ')] #punt 2
+last_name_len = len(player[player.find(' ')+1:]) #punt 3
+name_short = player[:1] + '.' + player[player.find(' '):] #punt4
+
+#punt 5
+chant2 = (first_name + '!' + ' ') * len(first_name)
+size = len(chant2)
+chant = chant2[:size - 1]
+good_chant = chant[-1] != ' '
+
+#count length from chant to check the values
+size_chant = len(chant)
+size_chant2 = len(chant2)
+
+
+print (chant + '\n')
+print (good_chant)
+
+
