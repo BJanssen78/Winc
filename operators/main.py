0dff3024bce11f58cb9264350ef815cf32efbcb3
BJanssen78 <98946596+BJanssen78@users.noreply.github.com>
2022-06-04 10:34:45 +0200

    updated operators

--- operators/main.py
+++ operators/main.py
@@ -19,20 +19,32 @@ populatie_spain = 47163418
 populatie_swits = 8508698
 
 #vergelijkingen
-fav_taal = fav_lang_Swits == fav_lang_Spain #01
-most_religions = most_religions_Spain == most_religions_Swits #02
-length_capital = (len(capital_Spain)!= len(capital_Swits)) #03
-swits_gdp_greater_than = gdp_swits > gdp_spain #04
+
+#01 The language spoken the most in Switzerland is the same as in Spain.
+fav_taal = fav_lang_Swits == fav_lang_Spain
+#02 The most prevalent religion in Switzerland is the same as in Spain.
+most_religions = most_religions_Spain == most_religions_Swits
+#03 The name length of Spain's capital does not equal that of Switzerland.
+length_capital = (len(capital_Spain)!= len(capital_Swits))
+#04 Switzerland's GDP is greater than Spain's GDP.
+swits_gdp_greater_than = gdp_swits > gdp_spain
+#05 The population growth is less than 1% in Switzerland and Spain.
 overall_growth_spain = growth_spain < '1%'
 overall_growth_swits = growth_swits < '1%'
+overall_growth = growth_spain and growth_swits < '1%'
+#06 At least one of the two countries has a population count of over 10 million.
 population_10mill_spain = populatie_spain > 10000000
 population_10mill_swits = populatie_swits > 10000000
+population = populatie_spain > 10000000 or populatie_swits >= 10000000
+
+#07 Exactly one of the two countries has a population count of over 10 million.
+population_at_least_one = populatie_swits and populatie_spain > 10000000
 
 #print uitkomst
 print (fav_taal) #01 false
 print(most_religions) #02 true
-print(swits_gdp_greater_than) #03 false
-print(overall_growth_spain) #04 false
-print(overall_growth_swits) #05 true
-print(population_10mill_spain) #06 
-print(population_10mill_swits)
+print (length_capital) #03 true
+print(swits_gdp_greater_than) #04 false
+print(overall_growth) #05 false
+print(population) #06 true
+print(population_at_least_one) #07 true
