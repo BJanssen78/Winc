6a0eae4a822af0afb16da3fc64ef7354536b7967
BJanssen78 <98946596+BJanssen78@users.noreply.github.com>
2022-06-24 21:43:23 +0200

    update conditions

--- conditions/main.py
+++ conditions/main.py
@@ -6,7 +6,6 @@ __human_name__ = 'conditions'
 
 #lijst met factors om te checken.
 
-
 f_weather = ['rainy','sunny','windy','neutral']
 f_time_of_day = ['day','night']
 f_cow_milking_status = ['need milking','Don\'t need milking']#True or False
@@ -14,27 +13,25 @@ f_location = ['pasture','cowshed']
 f_season = ['winter','spring','summer','fall']
 f_slurry_tank = ['Full','Not full']#True or False
 f_grass_status = ['Long','Short']#True or False
+
 #einde lijst, verwijderen na succes.
 
 def farm_action(weather,time_of_day,cow_milking_status,location,season,slurry_tank,grass_status):
-    if weather == 'rainy' or time_of_day == 'night' and location == 'pasture':
-        print('Take cows to cowshed')
-        action = 'Take cows to cowshed'
-        return action
-    elif cow_milking_status == True and location == 'cowshed':
-        print('milk cows')
-        action = 'milk cows' + '\n'
-        return action
-    elif weather != 'sunny' or weather != 'windy' and location == 'cowshed' and slurry_tank == True:
-        print('fertilize pasture')
-        action = 'fertilize pasture'
-        return action
-    elif weather == 'sunny' and season == 'spring' and grass_status == True and location != 'pasture':
-        print('mow grass')
-        action = 'mow grass'
-        return action
+    action = ''
+    if (weather == 'rainy' or time_of_day == 'night') and location == 'pasture':
+        action += 'Take cows to cowshed\n'
+    if cow_milking_status == True and location == 'cowshed':
+        action += 'milk cows\n'
+    if (weather != 'sunny' or weather != 'windy') and location == 'cowshed' and slurry_tank == True:
+        action += 'fertilize pasture\n'
+    if weather == 'sunny' and season == 'spring' and grass_status == True and location != 'pasture':
+        action += 'mow grass'
+        return (action)
+    if action != '': #extra controle
+        print('action is not empty\n',action) #extra toevoeging om te zien of wincpy nu werkt.
     else:
-        print('wait')
-        return False
-results = f"""{farm_action('sunny','day',True,'cowshed','spring',False,True)}
-{farm_action('sunny','day',True,'cowshed','spring',False,True)}"""
+        action = 'wait'
+        return (action)
+
+                
+print(farm_action('sunny','day',True,'pasture','spring',False,True))
