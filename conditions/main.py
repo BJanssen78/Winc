be7e34bb534610c6f3d663ef3657a88f43911fd9
Ben Janssen <98946596+BJanssen78@users.noreply.github.com>
2022-09-22 21:17:50 +0200

    Deleted files

--- conditions/main.py
+++ conditions/main.py
@@ -18,29 +18,36 @@ f_grass_status = ['Long','Short']#True or False
 
 def farm_action(weather,time_of_day,cow_milking_status,location,season,slurry_tank,grass_status):
     action = ''
+    # check of de koeien naar de schuur moeten
     if (weather == 'rainy' or time_of_day == 'night') and location == 'pasture':
-        action += 'take cows to cowshed\n'  
+        action += 'take cows to cowshed'  
+    # check of er gemolken moet worden
     if cow_milking_status == True and location == 'cowshed':
         action += 'milk cows'
         if weather != 'rainy':
-            action += '\ntake cows back to pasture'       
+            action += '\ntake cows back to pasture'  
+    # check op de juiste locatie            
     if cow_milking_status == True and location == 'pasture':
         action += 'take cows to cowshed\nmilk cows'
+        # extra check of ze terug moeten
         if weather != 'rainy' and slurry_tank == False:
-            action += '\ntake cows back to pasture'      
+            action += '\ntake cows back to pasture'    
+              
     if (weather != 'sunny' or weather != 'windy') and location == 'cowshed' and slurry_tank == True:
-        action += '1fertilize pasture'
+        action += 'fertilize pasture'
         if weather != 'rainy':
-            action += '\ntake cows back to pasture'      
+            action += '\ntake cows back to pasture'
+                  
     if (weather != 'sunny' or weather != 'windy') and location == 'pasture' and slurry_tank == True:
-        action += 'take cows to cowshed\nfertilize pasture'
+        action += '2take cows to cowshed\nfertilize pasture'
         if weather != 'rainy':
             action += '\ntake cows back to pasture'      
+            
     if weather == 'sunny' and season == 'spring' and grass_status == True and location != 'pasture':
         action += 'mow grass'
         return (action)   
     if action != '': #extra controle
-        print(action) #extra toevoeging om te zien of wincpy nu werkt.
+        # print(action) #extra toevoeging om te zien of wincpy nu werkt.
         return(action)
     else:
         action = 'wait'
@@ -48,4 +55,5 @@ def farm_action(weather,time_of_day,cow_milking_status,location,season,slurry_ta
 
                 
 # farm_action('sunny','day',True,'pasture','spring',False,True);
-print (farm_action('sunny','day',True,'pasture','spring',True,True));
+print (farm_action('sunny', 'day', True, 'cowshed', 'spring', False, True));
+
---
+++ lists/main.py
@@ -0,0 +1,51 @@
+# Do not modify these lines
+__winc_id__ = '6eb355e1a60f48a28a0bbbd0c88d9ab4'
+__human_name__ = 'lists'
+
+# Add your code after this line
+
+movie_title = ["The Poseidon Adventure",
+                "Cinderella Liberty",
+                "Tom Sawyer",
+                "Earthquake",
+                "Jaws",
+                "Close Encounters of the Third Kind",
+                "Star Wars: Episode IV A New Hope",
+                "Superman",
+                "Star Wars: Episode V The Empire Strikes Back",
+                "E.T. the Extra-Terrestrial",
+                "If We Were in Love(from Yes, Giorgio)",
+                "The River",
+                "Empire of the Sun",
+                "The Accidental Tourist",
+                "Born on the Fourth of July",
+                "Schindler's List",
+                "Moonlight(from Sabrina)",
+                "Seven Years in Tibet",
+                "Saving Private Ryan",
+                "Angela's Ashes",
+                "A.I. Artificial Intelligence",
+                "Memoirs of a Geisha",
+                "War Horse",
+                "Lincoln",
+                "The Book Thief",
+                "The Post"]
+
+golden_globe_winners = ["Jaws",
+                        "Star Wars: Episode IV A New Hope",
+                        "E.T. the Extra-Terrestrial",
+                        "Memoirs of a Geisha"]
+
+def alphabetical_order(ascend):
+    if ascend == "yes":
+        ascended_list = movie_title.sort(reverse=False)
+        return (ascended_list)
+
+def who_won_golden_globe():
+    if golden_globe_winners exists in movie_title
+        return True
+
+print (movie_title);
+print ("\n");
+print (golden_globe_winners);
+
